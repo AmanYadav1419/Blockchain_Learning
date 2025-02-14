b08474d06727eaf067081140f3a068919c74f804
unknown <amanyadav16544@gmail.com>
2025-02-14 14:09:58 +0530

    created all function's blank statment

--- Voting_SmartContract/Voting.sol
+++ Voting_SmartContract/Voting.sol
@@ -42,4 +42,70 @@ contract Vote {
     bool stopVoting; // this is for emergency situation to stop voting
 
     // constructor function , when smart contract get deployed this will call first.
+    constructor() {}
+
+    // created modifiers so that only the authoroised person can call the function
+    // first modifier for checking is voting over or not
+    modifier isVotingOver() {
+        
+        _;
+    }
+
+    // second modifier is for checking the only commisioner
+    modifier onlyCommisioner() {
+        
+        _;
+    }
+
+    // function for candidate registering.
+    function candidateRegister(
+        string calldata _name,
+        string calldata _party,
+        uint age,
+        string calldata _gender
+    ) external {
+
+    }
+
+    // function for verification of candidate, and it will return bool value, like true or false for the candidate is verified or not
+    function candidateVerification(address _person) internal view returns(bool) {
+
+    }
+
+    // function to return  all the candidates list.
+    function candidateList() public view returns(Candidate[] memory){
+
+    }
+
+    
+    // function for voter registering.
+    function voterRegister(string calldata _name, uint _age, string calldata _gender) external {
+
+    }
+
+    // function for vote
+    function vote(uint _voterId, uint _id) external {
+
+    }
+
+    // function for voteTime, which is only call by the election commisioner
+    function voteTime(uint _startTime, uint _endTime) external onlyCommisioner() {
+
+    }
+
+    // function for checking the vote status
+    function voteStatus() public view returns(string memory){
+
+    }
+
+    // function to declare the result of the election and which is only called by election commisioner
+    function result() external onlyCommisioner() {
+
+    }
+
+    // function to stop the voting in an emergency case which is only called by election commisioner
+    function emergency() onlyCommisioner(){
+        
+    }
+
 }
