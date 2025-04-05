26a29164a141dba0b9bf9e8e72a15adad235ed14
unknown <amanyadav16544@gmail.com>
2025-04-05 22:07:38 +0530

    done and completed the part of Events in Solidity

--- 07-Solidity_OOP's_Concepts_Learning/Event.sol
+++ 07-Solidity_OOP's_Concepts_Learning/Event.sol
@@ -16,6 +16,35 @@ contract Event_Demo{
 
         // to call the event we need to write "emit" keyword,
         // and after the event name with the data passed if arguments need to passed.
-        emit sendEthInformation(msg.sender, reciever, msg.value) 
+        emit sendEthInformation(msg.sender, reciever, msg.value);
     }
+
+    // to event ki jahag ky ham kuch dusre tarike se nahi kar sakte hai 
+    // here is the example without using event and emit
+    // so for returning mulitple values  we need to write each data type in returns, correponds to the data returning 
+    function sendEth1(address payable reciver) public payable returns(address, address, uint) {
+        reciver.transfer(msg.value);
+
+        // here the another way to show the all the data, rather than using event emit
+        // in solidity we can return multiple things at a time
+        return(msg.sender, reciver, msg.value)
+    }
+
+    // to uper code se bhi sab work ho raha hai
+    // to event ka kuch kamm hai kya , kuch useful hai kya ye questions aa sakte hai.
+    // to kuch to use hoga
+    // to sendEth and sendEth1 kya ye dono function same hai, to nahi ye bass dikhne me same lag rahe hai, but acutally both are different.
+    // events jo hai wo kaffi useful hote hai, specially tab jab jab aap koi third party application jase ki koi external system hai,
+    // jo ki ye detect kar raha hai ki smart contract me kuch hua ki nahi hua.
+
+    // and events me logs hota hai , like jo bhi kuch hua hamne kiya uska ek log maintained hoga.
+    // consider hamare smart contract ko hazaro log use kar rahe hai , to ham logs ke help ke through ye dekh sakte hai konse user ne kya kya kiya , 
+    // kitne ether transfer kiye and all the other information jo ki log me store ho rahi hai.  
+    // Hamara Gas cost kaffi bachta hai by using events so in comparing to returning some values in function with event and emit ,event emit required requires the less gas cost.
+    // storage cost of events is also less, rather than storing storing historical data in state variables.
+
+    // so in general the summary is there are three advantages of using events :-
+    // 1. External system communication medium 
+    // 2. Gas consumption
+    // 3. Historical data
 }
